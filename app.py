73abdd97a3df5cddcccdb31f474d5f419febf188
Matti Österbacka <matti.osterbacka@gmail.com>
2021-03-18 12:47:58 +0200

    registration and login functionalities and validation improved

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tsoha"
 db = SQLAlchemy(app)
 
@@ -18,10 +20,16 @@ def registerform():
 def registerresult():
     username = request.form["username"]
     password = request.form["password"]
+    sql = "SELECT username from users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user != None:
+        return "User name already taken"
     sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
     db.session.execute(sql, {"username":username, "password":password})
     db.session.commit()
-    return render_template("registerresult.html", username=request.form["username"])
+    session["username"] = username
+    return redirect("/")
 
 @app.route("/loginform")
 def loginform():
@@ -38,4 +46,10 @@ def login():
         return "No user with that name"
     elif user["password"] != password:
         return "Wrong password"
-    return render_template("loginresult.html", username=request.form["username"])
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
